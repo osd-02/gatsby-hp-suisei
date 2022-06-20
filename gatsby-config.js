511080c4d585f4d22833f4dfc3f6a9c5d0713d02
osd-02 <sakanaq0223@gmail.com>
2022-06-21 01:25:28 +0900

    contentfulとの連携

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,26 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+ require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
+  siteMetadata: {
+    title: "Dev Blog",
+    description: "Gatsbyで作成したブログサイトです。",
+    author: "Engineer X"
+  },
   /* Your site config here */
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_API_KEY
+      }
+    },
+    'gatsby-plugin-image',
+    `gatsby-plugin-sass`
+  ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,14 @@
 import React from "react"
 import "../styles/layout.scss"
 import Header from "./header"
+import Footer from "./footer"
 
 const Layout = ({ children }) => {
     return (
         <div className="layout">
             <Header />
             <main>{children}</main>
+            <Footer />
         </div>
     )
 }

1035d58b414ffeae80ee49b6d3b828879c32a8cf
osd-02 <sakanaq0223@gmail.com>
2022-06-25 21:47:40 +0900

    heroの削除

--- src/pages/postsList/live.js
+++ src/pages/postsList/live.js
@@ -1,13 +1,11 @@
 import React from "react"
 import Layout from "../../components/layout"
-import Hero from "../../components/hero"
 import PostLink from "../../components/post-link"
 import { graphql } from "gatsby"
 
 export default function Home({ data }) {
   return (
     <Layout>
-      <Hero />
       {data.allContentfulLivePost.edges.map(edge => (
         <PostLink key={edge.node.slug} post={edge.node} />
       ))}
